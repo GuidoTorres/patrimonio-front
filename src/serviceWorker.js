9e8457eb17e57bd601a92f37d466cc68a57088aa
unknown <gt12930@gmail.com>
2024-10-25 15:03:20 -0500

    fix: bienes

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,21 +1,21 @@
-// serviceWorker.js
 
-// Registramos el service worker para que funcione como una PWA
+
 export function register() {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-        navigator.serviceWorker
-          .register(swUrl)
-          .then((registration) => {
-            console.log('Service Worker registrado con éxito:', registration);
-          })
-          .catch((error) => {
-            console.error('Error al registrar el Service Worker:', error);
-          });
-      });
-    }
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+          console.log('Service Worker registrado:', registration);
+        })
+        .catch((error) => {
+          console.error('Error al registrar el Service Worker:', error);
+        });
+    });
   }
+}
+
   
   // Función para desactivar el service worker si es necesario
   export function unregister() {
